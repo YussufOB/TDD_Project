7670890387501158fe5dc52130906d06bcc4c4ae
FuadNabi <fuad.nebiyev95@mail.ru>
2023-02-16 13:41:55 +0400

    Fix lintern errors

--- solver.rb
+++ solver.rb
@@ -1,6 +1,6 @@
 class Solver
   def factorial(number)
-    raise Expection, "Negative numbers are not allowed" if number < 0
+    raise Expection, 'Negative numbers are not allowed' if number.negative?
     return 1 if number == 1
     return 1 if number.zero?
 
